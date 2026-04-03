5472b0ec8720f630d8d0efd63bf7721810c627c1
orytsukz-creator <orytsukz@gmail.com>
2026-04-03 20:19:10 -0300

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -2,7 +2,6 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
 
-
 local RRR = Instance.new("ScreenGui")
 RRR.Name = "RRR"
 RRR.Parent = playerGui
@@ -46,6 +45,8 @@ RRR.Parent = playerGui
         Drag.SliceScale = 1
         Drag.TileSize = UDim2.new(1, 0, 1, 0)
         Drag.Parent = Container
+		Drag.Active = true
+		Drag.Draggable = true
 
             local Main = Instance.new("ImageLabel")
             Main.Name = "Main"
@@ -1105,51 +1106,3 @@ RRR.Parent = playerGui
                 Minimize.AutoButtonColor = true
                 Minimize.Parent = UpBar
 
-local UIS = game:GetService("UserInputService")
-
-local frame = Drag -- << TROCA pelo seu frame principal
-
-local dragging = false
-local dragInput
-local dragStart
-local startPos
-
-local function update(input)
-	local delta = input.Position - dragStart
-	frame.Position = UDim2.new(
-		startPos.X.Scale,
-		startPos.X.Offset + delta.X,
-		startPos.Y.Scale,
-		startPos.Y.Offset + delta.Y
-	)
-end
-
-frame.InputBegan:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseButton1
-		or input.UserInputType == Enum.UserInputType.Touch then
-
-		dragging = true
-		dragStart = input.Position
-		startPos = frame.Position
-
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then
-				dragging = false
-			end
-		end)
-	end
-end)
-
-frame.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseMovement
-		or input.UserInputType == Enum.UserInputType.Touch then
-
-		dragInput = input
-	end
-end)
-
-UIS.InputChanged:Connect(function(input)
-	if input == dragInput and dragging then
-		update(input)
-	end
-end)
