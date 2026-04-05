2a7f466394befad525630303bbc16c68e023d0f5
orytsukz-creator <orytsukz@gmail.com>
2026-04-05 00:46:29 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,4 @@
--- 1. Carrega a Interface
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/orytsukz-creator/MonkysHub/refs/heads/main/HubRRR.lua"))()
-
--- 2. Espera a UI carregar as tabelas globais (IMPORTANTE!)
 repeat task.wait() until getgenv().RRR_Configs and getgenv().RRR_Configs.Keys
-
--- 3. Carrega o script com as funções (Adicionais.lua)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/orytsukz-creator/MonkysHub/refs/heads/main/Adicionais.lua"))()
