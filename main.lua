dada92c716ee3d86427ac2dd73616e0d09b8a938
orytsukz-creator <orytsukz@gmail.com>
2026-04-06 21:29:09 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/orytsukz-creator/MonkysHub/refs/heads/main/HubRRR.lua"))()
-repeat task.wait() until getgenv().RRR_Configs and getgenv().RRR_Configs.Keys
+repeat task.wait() until getgenv().RRR_Config and getgenv().RRR_Config.Misc
 loadstring(game:HttpGet("https://raw.githubusercontent.com/orytsukz-creator/MonkysHub/refs/heads/main/Adicionais.lua"))()
