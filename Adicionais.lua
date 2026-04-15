be14332bb29c9bbc4f95b73fda90ac592063d1b7
orytsukz-creator <orytsukz@gmail.com>
2026-04-15 00:29:47 -0300

    Update Adicionais.lua

--- Adicionais.lua
+++ Adicionais.lua
@@ -132,8 +132,8 @@ local function realizarChuteAutoGol()
     
     -- LÓGICA DINÂMICA (LIMITES: 0.07 - 0.15)
     local alturaMin = 0.07 -- Perto
-    local alturaMax = 0.15 -- Longe (400 studs)
-    local distanciaLimite = 400 
+    local alturaMax = 0.21 -- Longe (400 studs)
+    local distanciaLimite = 600
 
     local progresso = math.clamp(distancia / distanciaLimite, 0, 1)
     local alturaDinamica = alturaMin + (progresso * (alturaMax - alturaMin))
